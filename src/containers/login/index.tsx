4d6d3e40067ccd0561f0c9e226e49c46935cf387
user <user@users-MacBook-Pro.local>
2019-11-26 22:32:04 +0200

    basic login style

--- src/containers/login/index.tsx
+++ src/containers/login/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Button, Input } from "reactstrap";
 import "./styles.scss";
 
 interface Props {
@@ -6,7 +7,29 @@ interface Props {
 }
 
 function Login(props: Props) {
-  return <div className={"loginContainer"}>login</div>;
+  const [username, setUsername] = React.useState("");
+  const { onUserLogin } = props;
+  const onUsernameInputChange = (e: any) => {
+    const username = e.target.value;
+    setUsername(username);
+  };
+  return (
+    <div className={"loginContainer"}>
+      <div className={"loginTitle"}>Hi, what is your name?</div>
+      <div className={"loginInput"}>
+        <Input
+          type="text"
+          onChange={onUsernameInputChange}
+          placeholder="Please enter username"
+        />
+      </div>
+      <div className={"loginSubmit"}>
+        <Button color="info" onClick={() => onUserLogin(username)}>
+          Let me in!
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
